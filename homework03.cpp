eb6de1d8600d22242ba8374bce56fb4017c07e43
saamiyahali <98719747+saamiyahali@users.noreply.github.com>
2024-03-10 15:19:40 -0700

    Homework3

---
+++ homework03.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // display the welcome message
+    cout << "Welcome to my Word Game! This is the template:\n\n";
+
+    // display the template
+    cout << "This is the story of NOUN.\n";
+    cout << "They are about NUMBER years old.\n";
+    cout << "They like the color NOUN because \n";
+    cout << "it reminds them of NOUN.\n";
+    cout << "On weekends, they VERB because it makes them feel ADJECTIVE.\n";
+    cout << "They work as a stock market investor.\n";
+    cout << "Currently, they are focusing on a stock with price $NUMBER.\n";
+    cout << "The stock name is NOUN and uses the marker SYMBOL.\n\n";
+
+    // TODO: gather the input needed for the story
+
+    // TODO: display the story with the filled information
+
+    // TODO: seed the random number generator using the current system time
+
+    // TODO: create the graph
+
+    // TODO: display the final stock price
+
+    // terminate
+    return 0;
+}
